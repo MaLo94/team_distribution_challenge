7bd203bce6e6c18c7effc0af0fde0b35547c31e2
MaLo94 <mahdilotfi.j444@gmail.com>
2023-05-22 11:03:19 +0330

    structure modified

---
+++ model/kmeans.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np 
+from matplotlib import pyplot as plt
+
+class xxx:
+    def __init__(self, path:str, k:int = 5) -> None:
+        self.path = path
+        self.num_team = k
+
+    def Dataloader():
+        pass
+
+    def Dataplotter():
+        pass
---
+++ train.py
@@ -0,0 +1,4 @@
+# To see the result you should run this code with proper args
+
+if __name__ == "__main__":
+    print('result will be appeared in this module')
