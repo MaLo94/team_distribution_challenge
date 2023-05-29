6e22bb6601111d8d69283b8a74f83666bb58cfca
MaLo94 <mahdilotfi.j444@gmail.com>
2023-05-29 15:31:15 +0330

    model class, dataplotter and dataloader added

--- model/kmeans.py
+++ model/kmeans.py
@@ -1,14 +1,30 @@
 import pandas as pd
 import numpy as np 
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
-class xxx:
+
+class DistributionModifier:
+
     def __init__(self, path:str, k:int = 5) -> None:
         self.path = path
         self.num_team = k
 
-    def Dataloader():
-        pass
+    def dataloader(self):
 
-    def Dataplotter():
-        pass
+        data = pd.read_csv(self.path)
+        y = data.iloc[:, 0].values
+        x = data.iloc[:, 1].values
+        self.samples = np.column_stack((x, y))
+        return x , y, self.samples
+
+    def making_plot_from_raw_data(self):
+
+        x, y, samples = self.dataloader()
+        plt.scatter(x, y)
+        plt.xlabel('Feature 2')
+        plt.ylabel('Feature 1')
+        plt.title('Distribution of Samples')
+        plt.show()
+
+    def initialize_centroids(self):
+        self.centroids = self.samples[np.random.choice(range(len(self.samples)), self.k, replace=False)]
--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 # To see the result you should run this code with proper args
-from model.kmeans import xxx
+from model.kmeans import *
 
 if __name__ == "__main__":
+    model = DistributionModifier(path='dataset/rc_task_2.csv')
+    model.making_plot_from_raw_data()
     print('result will be appeared in this module')
